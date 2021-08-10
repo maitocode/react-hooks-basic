1ff39c744d4088c53eb8da98b11c27a7ee4c3c8d
bluerain4 <code.maito@gmail.com>
2021-08-10 18:24:50 +0700

    List Post Remove

--- src/App.js
+++ src/App.js
@@ -1,13 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
-import ColorBox from "./components/ColorBox/index.jsx";
+import PostList from "./components/PostList/index.jsx";
+
+import TodoForm from "./components/TodoForm/index.jsx";
+import TodoList from "./components/TodoList/index.jsx";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "i love Trinh haha!" },
+    { id: 2, title: "i love Chin haha!" },
+    { id: 3, title: "i love Ching haha!" },
+  ]);
+
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl =
+          "http://js-post-api.herokuapp.com/api/posts?limit=10&_page=1";
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        console.log({ responseJSON });
+
+        const { data } = responseJSON;
+        setPostList(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    fetchPostList();
+  }, []);
+
+  useEffect(() => {
+    console.log("TODO list effect");
+  });
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex((x) => x.id === todo.id);
+
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
+  function handleTodoFormSubmit(formValues) {
+    console.log("Form submit: ", formValues);
+    const length = todoList.length;
+    const newTodo = {
+      id: length + 1,
+      ...formValues,
+    };
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <h1>Welcome to React Hooks!</h1>
+      <h1>React Hooks - To do list</h1>
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
 
-      <ColorBox />
+      <PostList posts={postList} />
     </div>
   );
 }
